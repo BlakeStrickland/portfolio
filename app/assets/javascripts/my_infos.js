4e742a4f415c5d1ed5d3473666a57284062d97e0
Blake Strickland <Stricklandb02@gmail.com>
2016-04-25 20:18:34 -0400

    tabs and footer

--- app/assets/javascripts/my_infos.js
+++ app/assets/javascripts/my_infos.js
@@ -1,33 +1,52 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function() {
-  if ($("js-parallax-window").length) {
-    parallax();
-  }
-});
+// $(document).ready(function() {
+//   if ($("js-parallax-window").length) {
+//     parallax();
+//   }
+// });
+//
+// $(window).scroll(function(e) {
+//   if ($("#js-parallax-window").length) {
+//     parallax();
+//   }
+// });
+//
+// function parallax(){
+//   if( $("#js-parallax-window").length > 0 ) {
+//     var plxBackground = $("#js-parallax-background");
+//     var plxWindow = $("#js-parallax-window");
+//
+//     var plxWindowTopToPageTop = $(plxWindow).offset().top;
+//     var windowTopToPageTop = $(window).scrollTop();
+//     var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+//
+//     var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+//     var windowInnerHeight = window.innerHeight;
+//     var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+//     var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+//     var plxSpeed = 0.35;
+//
+//     plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+//   }
+// }
 
-$(window).scroll(function(e) {
-  if ($("#js-parallax-window").length) {
-    parallax();
-  }
-});
-
-function parallax(){
-  if( $("#js-parallax-window").length > 0 ) {
-    var plxBackground = $("#js-parallax-background");
-    var plxWindow = $("#js-parallax-window");
-
-    var plxWindowTopToPageTop = $(plxWindow).offset().top;
-    var windowTopToPageTop = $(window).scrollTop();
-    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
-
-    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
-    var windowInnerHeight = window.innerHeight;
-    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
-    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
-    var plxSpeed = 0.35;
-
-    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
-  }
-}
+// $(document).ready(function () {
+//   $('.accordion-tabs-minimal').each(function(index) {
+//     $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+//   });
+//   $('.accordion-tabs-minimal').on('click', 'li > a.tab-link', function(event) {
+//     if (!$(this).hasClass('is-active')) {
+//       event.preventDefault();
+//       var accordionTabs = $(this).closest('.accordion-tabs-minimal');
+//       accordionTabs.find('.is-open').removeClass('is-open').hide();
+//
+//       $(this).next().toggleClass('is-open').toggle();
+//       accordionTabs.find('.is-active').removeClass('is-active');
+//       $(this).addClass('is-active');
+//     } else {
+//       event.preventDefault();
+//     }
+//   });
+// });
