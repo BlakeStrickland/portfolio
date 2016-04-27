cb0d68709e5a6723d7adf681030999683c1504aa
Blake Strickland <Stricklandb02@gmail.com>
2016-04-27 09:27:40 -0400

    Add modal for resume

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -60,3 +60,21 @@ function parallax(){
     plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
   }
 }
+
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
